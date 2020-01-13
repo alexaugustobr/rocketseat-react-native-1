8ddb0c8fd15b844df3eac58afe4600577fef7b46
alexcologia33 <alexcologia33@gmail.com>
2020-01-12 21:31:35 -0300

    aula 10

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ npx react-native run-android
 
 react-native start
 
+cd ~/Android/Sdk/tools/bin && ./avdmanager list avd
+
+cd ~/Android/Sdk/tools && ./emulator -avd Nexus_5X_API_28
+
+--npm i -g react-native-cli
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
 import Main from './pages/main';
+import Product from './pages/product';
 
 const AppStackNavigator = createStackNavigator({
-        Main
+        Main,
+        Product
     }, 
     {
-        initialRouteName: 'Main',
         defaultNavigationOptions: {
             headerStyle: {
                 backgroundColor: "#DA552F"
